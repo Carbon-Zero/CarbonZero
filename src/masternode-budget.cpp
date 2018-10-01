85d9edf6d93bb917bb39a3e8ba028c5f7629328c
Carbon-Zero <42657652+Carbon-Zero@users.noreply.github.com>
2018-10-01 19:17:23 -0400

    Update masternode-budget.cpp

--- src/masternode-budget.cpp
+++ src/masternode-budget.cpp
@@ -975,7 +975,13 @@ CAmount CBudgetManager::GetTotalBudget(int nHeight)
     }
 
     // Amount of blocks in a months period of time (using 1 minutes per) = (60*24*30)
-return ((nSubsidy / 100) * 10) * 1440 * 30;
+  if (nHeight == 0) {
+        return 0 * COIN;
+    } else {
+        return ((nSubsidy / 100) * 10) * 1440 * 30;
+    }
+}
+
         
 
 }
