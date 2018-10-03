bea6e1784e091240934b3a8152ffd4aa8708ab57
Carbon-Zero <42657652+Carbon-Zero@users.noreply.github.com>
2018-10-02 21:59:57 -0400

    Update version.h

--- src/version.h
+++ src/version.h
@@ -11,20 +11,20 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70921;
+static const int PROTOCOL_VERSION = 70922;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70920;
+static const int GETHEADERS_VERSION = 70922;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70921;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70921;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70922;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70922;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70921;
+static const int MIN_PEER_MNANNOUNCE = 70922;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
