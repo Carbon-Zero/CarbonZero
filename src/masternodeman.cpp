7d35f66ab70660c1f18c0e080895fb31be65dbde
Carbon-Zero <42657652+Carbon-Zero@users.noreply.github.com>
2018-10-05 01:36:58 -0400

    Update masternodeman.cpp

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -13,7 +13,7 @@
 #include <boost/filesystem.hpp>
 #include <boost/lexical_cast.hpp>
 
-#define MN_WINNER_MINIMUM_AGE 4000    // Age in seconds. This should be > MASTERNODE_REMOVAL_SECONDS to avoid misconfigured new nodes in the list.
+#define MN_WINNER_MINIMUM_AGE 8000    // Age in seconds. This should be > MASTERNODE_REMOVAL_SECONDS to avoid misconfigured new nodes in the list.
 
 /** Masternode manager */
 CMasternodeMan mnodeman;
