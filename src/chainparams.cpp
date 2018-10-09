9a62f0a881e5370fcf35d9c18bdb4bf6cbd60755
Carbon-Zero <42657652+Carbon-Zero@users.noreply.github.com>
2018-10-09 15:59:50 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -140,7 +140,7 @@ public:
         nBlockLastGoodCheckpoint = 1; // Accumulators will be valid from launch
         nBlockEnforceInvalidUTXO = 1; // Enforce valid UTXOs from launch
         nInvalidAmountFiltered = 0*COIN; // No coins were filtered through exchanges that should be considered valid. This was specific to PIVX
-        nBlockZerocoinV2 = 250; //!> ZeroCoin 2 needs extensive testing - TBD
+        nBlockZerocoinV2 = 9999999; //!> ZeroCoin 2 needs extensive testing - TBD
         nEnforceNewSporkKey = 1538719200; //!> Spork Keys signed after launch will be valid.
         nRejectOldSporkKey = 1538719200; //!> From Launch. We've used a dummy sporkkey as the old one since we are launching with the only one
 
