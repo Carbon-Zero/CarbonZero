ceb961d78113dd9b35bb441c428b01f5019fc480
Carbon-Zero <42657652+Carbon-Zero@users.noreply.github.com>
2018-10-26 15:07:46 -0400

    Disable Privacy Tab

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -664,7 +664,7 @@ void BitcoinGUI::setWalletActionsEnabled(bool enabled)
     overviewAction->setEnabled(enabled);
     sendCoinsAction->setEnabled(enabled);
     receiveCoinsAction->setEnabled(enabled);
-    privacyAction->setEnabled(disabled);
+    privacyAction->setEnabled(_disable);
     historyAction->setEnabled(enabled);
     QSettings settings;
     if (settings.value("fShowMasternodesTab").toBool()) {
