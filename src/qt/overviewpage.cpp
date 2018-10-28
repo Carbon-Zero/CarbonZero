1fec770e062efb3fe84c6896f828388f22c91c1b
Carbon-Zero <42657652+Carbon-Zero@users.noreply.github.com>
2018-10-28 19:47:20 -0400

    Update overviewpage.cpp

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -230,7 +230,7 @@ void OverviewPage::setBalance(const CAmount& balance, const CAmount& unconfirmed
 
     // zcarbon labels
     // ui->labelzBalance->setText(BitcoinUnits::floorHtmlWithUnit(nDisplayUnit, zerocoinBalance, false, BitcoinUnits::separatorAlways));
-    ui->labelzBalanceUnconfirmed->setText("COMING SOON!"& nHeight);
+    ui->labelzBalanceUnconfirmed->setText(nHeight);
     ui->labelzBalanceMature->setText("COMING SOON!");
     ui->labelzBalanceImmature->setText("COMING SOON!");
 
