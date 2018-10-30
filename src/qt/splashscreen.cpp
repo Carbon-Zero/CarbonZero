57f6cf3a25e91944fabf127bdd6c78280d5ec23f
Carbon-Zero <42657652+Carbon-Zero@users.noreply.github.com>
2018-10-29 22:17:35 -0400

    Update splashscreen.cpp

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -29,7 +29,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
     int paddingLeft = 14;
     int paddingTop = 470;
     int titleVersionVSpace = 17;
-    int titleCopyrightVSpace = 20;
+    int titleCopyrightVSpace = 24;
 
     float fontFactor = 1.0;
 
