69ea46c99b0f3de0cda013b4ae4efa0b7b65eeee
Carbon-Zero <42657652+Carbon-Zero@users.noreply.github.com>
2018-10-29 22:15:25 -0400

    Adjust copyrights

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -29,7 +29,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle* networkStyle) 
     int paddingLeft = 14;
     int paddingTop = 470;
     int titleVersionVSpace = 17;
-    int titleCopyrightVSpace = 44;
+    int titleCopyrightVSpace = 20;
 
     float fontFactor = 1.0;
 
