db8984dd5438c66f69bf8ff448f11cc62a97e7d4
Carbon Zero <42657652+Carbon-Zero@users.noreply.github.com>
2019-03-18 18:47:53 -0400

    Update invalid_outpoints.json.h

--- src/invalid_outpoints.json.h
+++ src/invalid_outpoints.json.h
@@ -13,6 +13,10 @@ std::string LoadInvalidOutPoints()
             "    \"txid\": \"2081a66031b8c726b50c25cc3cec89a982b2d96adda5a30013cf765475347bd2\",\n"
             "    \"n\": 0\n"
             "  },\n"
+            "  {\n"
+            "    \"txid\": \"6395435963ac9e18fab3919796f596eceecb5ec5baa96fe17b11111c9f055a4c\",\n"
+            "    \"n\": 0\n"
+            "  },\n"        
             "]";
     return str;
 }
