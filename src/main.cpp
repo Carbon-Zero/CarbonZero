fbcee68e7e95495e98525e5e2c1c209dc844c951
Carbon Zero <42657652+Carbon-Zero@users.noreply.github.com>
2019-03-24 00:44:17 -0400

    Typo

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-ermint// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2014-2015 The Dash developers
 // Copyright (c) 2014-2018 The PIVX Developers
